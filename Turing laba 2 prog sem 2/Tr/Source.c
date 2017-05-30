3d36ff90ba3b0009da3d7f87b8d9db87b1ac3ada
SGmuwa <motherlode.muwa@gmail.com>
2017-05-30 18:17:46 +0300

    Загрузка наработок в GitHub

---
+++ Turing laba 2 prog sem 2/Tr/Source.c
@@ -0,0 +1,440 @@
+#pragma once
+
+/* Secure Development Lifecycle */
+//#ifndef SDL
+/* Поставьте SDL 0, если Ваш компилятор не поддерживает данную технологию. */
+//#define SDL 1
+//#endif /* !SDL */
+
+//#include <locale.h>
+
+#ifndef _TURING_MACHINE_H_MUWA_LABA
+#define _TURING_MACHINE_H_MUWA_LABA
+
+#include "../../Turing/Muwa_expandable_lenta.h"
+
+#ifndef _INC_STDIO
+#include <stdio.h>
+#endif /* _INC_STDIO */
+
+#ifndef _INC_STRING
+#include <string.h>
+#endif /* _INC_STRING */
+
+#ifndef _INC_STDLIB
+#include <stdlib.h>
+#endif
+enum commands
+{
+	error,		/* При чтении возникла ошибка */
+	comment,	/* * - строка-комментарии */
+	movl,		/* Движение влево */
+	movr,		/* Движение вправо */
+	inc,		/* Добавить в ячейку 1 */
+	dec,		/* Убавить из ячейки 1 */
+	print,		/* Вывод ячейки */
+	get,		/* Считать 1 байт с клавиатуры и записать в текущую ячейку */
+	printc,		/* Вывод значения текущей ячейки как символ */
+	begin,		/* переход до end, если в ячейке 0 */
+	end			/* Переход вврх до begin, если в ячейке 1 */
+};
+
+/* Данная функция вернёт команду, которая первая в строке *input. Передаётся char** чтобы можно было передвигать char** */
+enum commands ReadCommand(char ** input)
+{
+	char buffer[32] = { '\0' };
+	sscanf(*input, "%32s", buffer);
+	if (buffer[0] != NULL) // Если buffer был заполнен
+	{
+		*input += strlen(buffer); // Добавляем то, что проглатили
+		if(**input == ' ') *input++;
+		if (strcmp(buffer, "movl") == 0)
+		{
+			return movl;
+		}
+		else if (strcmp(buffer, "movr") == 0)
+		{
+			return movr;
+		}
+		else if (strcmp(buffer, "inc") == 0)
+		{
+			return inc;
+		}
+		else if (strcmp(buffer, "dec") == 0)
+		{
+			return dec;
+		}
+		else if (strcmp(buffer, "print") == 0)
+		{
+			return print;
+		}
+		else if (strcmp(buffer, "get") == 0)
+		{
+			return get;
+		}
+		else if (strcmp(buffer, "printc") == 0)
+		{
+			return printc;
+		}
+		else if (strcmp(buffer, "begin") == 0)
+		{
+			return begin;
+		}
+		else if (strcmp(buffer, "end") == 0)
+		{
+			return end;
+		}
+		else if (strcmp(buffer, "*") == 0)
+		{
+			while(**input != '\n' || **input != EOF || **input != '\0') *input++;
+			return comment;
+		}
+		else return error;
+	}
+	else return error;
+}
+/* Структура хранит в себе команды и количество комманд */
+typedef struct
+{
+	enum commands * A;	/* Указывает на массив команд */
+	size_t c;			/* Количество команд в массиве */
+} commands_count;
+
+typedef struct
+{
+	char * A; // Array
+	size_t c; // count
+} char_count;
+
+// Функция преобразует текст в набор команд. Используется malloc и realloc в каждой итерации.
+/*
+char_count fData - данные, которые надо преобразовать в enum commands
+commands_count * out - адрес, куда должны быть помещены данные. Используется Malloc для out->A
+Возвращает: код ошибки.
+*/
+/*
+Ошибки:
+-1 - malloc(0) вернул NULL.
+-2 - realloc вернул NULL
+Успешные ситуации:
+0
+*/
+errno_t ReadsCommands(const char_count fData, commands_count * out)
+{
+// enum commands ReadCommand(char ** input)
+	if(fData.A == NULL) return -1;
+	char * index = fData.A;
+	enum commands buffer;
+	enum commands * tmp; // realloc
+	out->A = (enum commands*)malloc(0*sizeof(enum commands*));
+	if(out->A == NULL) return -1;
+	out->c = 0;
+	while(index < fData.c*sizeof(char) + fData.c)
+	{
+		buffer = ReadCommand(&index);
+		if(buffer != error)
+		{
+			tmp = NULL;
+			tmp = (enum commands *)realloc(out->A, out->c*sizeof(enum commands*));
+			if(tmp != NULL)
+			{
+				out->A = tmp;
+				out->c++;
+			}
+			else return -2; // error, нехватает памяти
+		}
+	}
+	return 0;
+}
+
+// Присваивает все элементы массива становятся копией объекта prototype
+// Массив A, образец prototype, размер одного элемента size, количество элементов count
+void ArraySetRage(void * A, void * prototype, size_t size, size_t count)
+{
+	size_t i, j;
+	for(i = 0; i < count; i++)
+	{
+		for(j = 0; j < size; j++)
+		{
+			((char*)A)[i*size + j] = ((char*)prototype)[j];
+		}
+	}
+}
+
+#ifndef _INC_FCNTL
+#include <fcntl.h>
+#endif
+
+#ifndef _fstat
+#include <sys/stat.h>
+#endif
+
+// Используется malloc. Считывает все данные с файла
+// fname - имя файла, который нужно открыть и прочитать
+// str - указатель на строку, куда надо записывать всё, что было прочитано с файла
+// str_size - сюда указывется размер файла. Возвращается, когда функция возвращает значения -4, 0, 1, 2, 3
+// Возвращаемое значение: код ошибки.
+// Ошибки:
+// -1 Не удалось открыть дескриптор для чтения
+// -2 Не удалось открыть файл для бинарного чтения
+// -3 Не удалось получить статитсику файла, о его размере
+// -4 Не удалось выделить оперативную память для помещения содержимого файла в оперативную память
+// Предупреждения:
+// 2 Не только не совпадает количество прочитанной информации, но и размер выделенной памяти оказался 0.
+// 1 Прочитанное количество элементов не совподает с размером файла.
+// 3 Было успешно выделено 0 байт в оперативную память.
+// Успешные ситуации:
+// 0 Успешно удалось поместить содержимое файла fname в участо памяти, на который теперь указывает *str, а размер содержимого помещён в *str_size.
+errno_t ReadAllDataFromFile(const char * fname, char ** str, size_t * str_size)
+{
+
+	char * buffer = NULL;
+
+	FILE * file = fopen(fname, "rb");
+
+	if (file != NULL)
+	{
+		fseek(file, 0L, SEEK_END);
+		*str_size = ftell(file);
+		buffer = (char*)malloc(*str_size);
+		if (buffer == NULL)
+		{
+			fclose(file);
+			return -4; // Не хватает памяти, но количество памяти удалось прочесть.
+		}
+		buffer[0] = '\0';
+		ArraySetRage(buffer, buffer, sizeof(char), *str_size); // Инцилизация массива
+		if (fread(buffer, sizeof(char), *str_size / sizeof(char), file) != sizeof(char)**str_size)
+		{
+			fclose(file);
+			*str = buffer;
+			if (*str_size == 0) return 2; // Не только не совпадает, но и размер выделенной памяти оказался 0.
+			else return 1; // warning: прочитанное количество элементов не совподает с размером файла.
+		}
+		fclose(file);
+		*str = buffer;
+		if (*str_size == 0) return 3; // Было успешно выделено 0 байт в оперативную память.
+		else return 0;
+	}
+	else
+	{
+		return -2; // Не удалось октрыть файл для бинарного чтения
+	}
+}
+
+// field - поле со списоком команд. Требуется для being и end
+// com - указатель на команду из field, которую следует обработать
+// update - лента, на которую помещена вся память
+// Возвращаемое значение:
+/*
+-1 Комманда com вне границах массива field.A .. field.A + field.c - 1
+0 Всё сработало нормально
+1 Не понятно, что делать с данной командой
+*/
+errno_t Step(const commands_count field, enum commands ** com, struct Lenta * update)
+{
+	if(*com < field.A || *com > field.A + field.c - 1) return -1;
+	ExpandableLenta_MemoryAdjustment(update);
+	switch (**com)
+	{
+	case movl:
+		update->position--;
+		*com++;
+		return 0;
+	case movr:
+		update->position++;
+		*com++;
+		return 0;
+	case inc:
+		update->position[0]++;
+		*com++;
+		return 0;
+	case dec:
+		update->position[0]--;
+		*com++;
+		return 0;
+	case printc:
+		printf("%c\n", update->position[0]);
+		*com++;
+		return 0;
+	case begin:
+		if (update->position[0] == 0)
+		{
+			// переход в end + 1
+			int find = 1;
+			while(find != 0)
+			{
+				*com++;
+				if(/* *com < field.A || */*com > field.A + field.c - 1) return -1;
+				if(**com == begin) find++;
+				else if(**com == end) find--;
+			}
+			*com++;
+			return 0;
+		}
+		else
+		{
+			*com++;
+			return 0;
+		}
+		return 0;
+	case end:
+		if (update->position[0] != 0)
+		{
+			// переход обратно на родственную begin + 1.
+			int find = -1;
+			while(find != 0)
+			{
+				*com--;
+				if(*com < field.A/* || *com > fild.A + field.c - 1*/) return -1;
+				if(**com == begin) find++;
+				else if(*com == end) find--;
+			}
+			*com++;
+			return 0;
+		}
+		else
+		{
+			*com++;
+			return 0;
+		}
+		return 0;
+	default:
+		*com++;
+		return 1;
+	}
+}
+
+void UserInterface()
+{
+//errno_t ReadsCommands(const char_count fData, commands_count * out)
+	char fname[_MAX_PATH] = {'\0'};
+	char_count fData = {NULL, 0};
+	errno_t er = 0; // ошибки работы
+	commands_count fCommands = {NULL, 0};
+	struct Lenta lt = {NULL, NULL, NULL};
+	enum commands * iCom = NULL; // Указатель (индекс) на текущую команду
+
+reset:
+	// Получение данных из файла
+
+	printf("Hello. Input filename: ");
+	do
+	{
+		if(er != 0) printf("error code: %d\n. Please, again.", (int)er);
+		gets_s(fname, _MAX_PATH);
+		er = ReadAllDataFromFile(fname, &(fData.A), &(fData.c));
+	} while(er < 0); // Считываем все данные с файла
+	if(er > 0) printf("filesize = 0 or read error\nWarning code: %d\n", (int)er);
+
+	er = 0;
+	// Преобразование данных файла в комманды
+	er = ReadsCommands(fData, &fCommands);
+	if(er < 0)
+	{
+		printf("char->commands: Error code: %d\nProgram reset...", (int)er);
+		er = 0;
+		free(fData.A);
+		if(fCommands.A != NULL) free(fCommands.A);
+		fData.c = 0; fCommands.c = 0;
+		fname[0] = '\0';
+		goto reset;
+	}
+	free(fData.A); fData.A = NULL; fData.c = 0;
+	
+	// Прохождение шагов
+	lt = ExpandableLenta_Create();
+	if(lt.position = NULL)
+	{
+		printf("ExpandableLenta_Create() return NULL (malloc)\nProgram reset...", (int)er);
+		er = 0;
+		free(fData.A);
+		if(fCommands.A != NULL) free(fCommands.A);
+		fData.c = 0; fCommands.c = 0;
+		fname[0] = '\0';
+		goto reset;
+	}
+//errno_t Step(const commands_count field, enum commands * com, struct Lenta * update)
+	iCom = fCommands.A;
+	
+	while(Step(fCommands, &iCom, &lt) >= 0);
+	free(fCommands.A); fCommands.A = NULL; fCommands.c = 0;
+	free(lt.elements); lt.elements = lt.position = lt.top = NULL;
+	return;
+}
+
+#endif _TURING_MACHINE_H_MUWA_LABA
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*enum commands * ReadsCommands(FILE * file, int * count)
+{
+	char * buffer[1024];
+	int i = 0;
+	for (; i < 1024; i++)
+	{
+		buffer[i] = (char*)malloc(sizeof(char)*256);
+		buffer[i][0] = 0;
+	}
+	i = 0;
+	//Чтение (построчно) данных из файла в бесконечном цикле
+	while (fscanf(file, "%s", buffer[i])) i++;
+	*count = i;
+	enum commands * output = (enum commands *)malloc(sizeof(enum commands) * *count);
+	for (; i >= 0; i--)
+	{
+		if (strcmp(buffer[i], "movl") == 0)
+		{
+			output[i] = movl;
+		}
+		else if (strcmp(buffer[i], "movr") == 0)
+		{
+			output[i] = movr;
+		}
+		else if (strcmp(buffer[i], "inc") == 0)
+		{
+			output[i] = inc;
+		}
+		else if (strcmp(buffer[i], "dec") == 0)
+		{
+			output[i] = dec;
+		}
+		else if (strcmp(buffer[i], "print") == 0)
+		{
+			output[i] = print;
+		}
+		else if (strcmp(buffer[i], "get") == 0)
+		{
+			output[i] = get;
+		}
+		else if (strcmp(buffer[i], "printc") == 0)
+		{
+			output[i] = printc;
+		}
+		else if (strcmp(buffer[i], "begin") == 0)
+		{
+			output[i] = begin;
+		}
+		else if (strcmp(buffer[i], "end") == 0)
+		{
+			output[i] = end;
+		}
+		else output[i] = error;
+	}
+	return output;
+}*/
---
+++ Turing/muwa_expandable_lenta.h
@@ -0,0 +1,243 @@
+﻿/* Разработал: Сидоренко Михаил (Sidorenko Mikhail), 15.03.2017 */
+
+
+#ifndef _MUWA_EXPANDABLE_LENTA
+#define _MUWA_EXPANDABLE_LENTA
+
+/* Включите этот параметр, если код не должен содержать XML документацию и комментарии //. */
+//#define C89
+
+/* Secure Development Lifecycle */
+#ifndef SDL
+#ifdef C89
+#define SDL 0
+#endif /* C89 */
+
+/* Поставьте SDL 0, если Ваш компилятор не поддерживает данную технологию. */
+#define SDL 1
+
+#endif // !SDL
+
+#ifndef _INC_STRING
+#include <string.h>
+#endif // _INC_STRING
+
+#ifndef _INC_STDLIB
+#include <stdlib.h>
+#endif // _INC_STDLIB
+
+#ifndef C89
+/// <summary>
+/// Структура, которая представляет собой указатель на начальный байт, конечный доступный и на текущий.
+/// </summary>
+struct Lenta
+#else
+struct Lenta
+#endif
+{
+	unsigned char * elements;	/* Начальный байт массива.				*/
+	unsigned char * position;	/* Текущий байт массива.				*/
+	unsigned char * top;		/* Последний доступный байт массива.	*/
+};
+
+char * ExpandableLenta_GetText(struct Lenta input, char empty)
+{
+	char * output = (char*)malloc(sizeof(char)*(input.top - input.elements + 1 + 1));
+	int i = 0;
+	for (; i < input.top - input.elements + 1; i++)
+	{
+		if (input.elements[i] != '\0')
+		{
+			output[i] = input.elements[i];
+		}
+		else
+		{
+			output[i] = empty; /* Знак лямбды */
+		}
+	}
+	output[input.top - input.elements + 1] = 0;
+	return output;
+}
+
+/* Функция создаёт начальную ленту из 5 ячеек. Текущим выбирается третий из них. */
+struct Lenta ExpandableLenta_Create(void)
+{
+	struct Lenta output = { (unsigned char *)malloc(sizeof(unsigned char) * 5), NULL, NULL };
+	int i = 0;
+	for (; i < 5; i++)
+	{
+		output.elements[i] = 0;
+	}
+	output.position = output.elements + 2;
+	output.top = output.elements + 4;
+	return output;
+}
+
+/* Функция создаёт ленту с указаным словом
+// text: Слово, которое должно быть помещено	*/
+struct Lenta ExpandableLenta_CreateText(char * text)
+{
+	size_t count = strlen(text);
+	size_t i = 0;
+	struct Lenta output = { (unsigned char *)malloc(sizeof(unsigned char) * (5 + count)), NULL, NULL };
+	output.position = output.elements + 2;
+	output.top = output.elements + 4 + count;
+
+	output.elements[5 + count - 1] = 0;
+	output.elements[5 + count - 2] = 0;
+	output.elements[5 + count - 3] = 0;
+	output.elements[0] = 0;
+	output.elements[1] = 0;
+
+	for (i = 2; i < 2 + count; i++)
+	{
+		output.elements[i] = text[i - 2];
+	}
+	return output;
+}
+
+#ifndef C89
+/// <summary>
+/// Проверка линии в машине Тьюринга, хватает ли места. Если не хватает, то идёт выделение новой памяти для ленты в машине Тьюринга. При неудаче возвращается отрицательный код ошибки.
+/// </summary>
+/// <param name="input">Состояние ленты Тьюринга</param>
+/// <return>Возвращает код, симвализирующий отчёт работы.
+/// 0	Выделение памяти не потребовалось
+/// 1	Память успешно выделена в сторону начала памяти
+/// 2	Память успешно выделена в сторону убывания памяти
+/// 3	Память успешно выделена в обе стороны
+/// -1	Локальная ошибка определения требований к выделению памяти. Состояние машины Тьюринга не изменено
+/// -2	Ошибка при выделении памяти в количестве sizeof(unsigned char)*(input->top - input->elements + 1 + 2) байт.  Состояние машины Тьюринга не изменено
+/// -3	Ошибка при выделении памяти в обе стороны в количестве sizeof(unsigned char)*(input->top - input->elements + 1 + 4) байт.  Состояние машины Тьюринга не изменено.
+/// -4	memcpy_s: Определено, что не хватает памяти в сторону убывания памяти, но возникла ошибка при копировании данных из старого сегмента памяти в новый. При этой ошибке новая память помечается свободной автоматически, изменения не принимаются в силу. Компиляция программы с технологией SDL.
+/// -5	memcpy: Определено, что не хватает памяти в сторону убывания памяти, но возникла ошибка при копировании данных из старого сегмента памяти в новый. При этой ошибке новая память помечается свободной автоматически, изменения не принимаются в силу.
+/// -6	memcpy_s: Определено, что не хватает памяти в сторону возрастания памяти, но возникла ошибка при копировании данных из старого сегмента памяти в новый. При этой ошибке новая память помечается свободной автоматически, изменения не принимаются в силу. Компиляция программы с технологией SDL.
+/// -7	memcpy: Определено, что не хватает памяти в сторону возрастания памяти, но возникла ошибка при копировании данных из старого сегмента памяти в новый. При этой ошибке новая память помечается свободной автоматически, изменения не принимаются в силу.
+/// -8	memcpy_s: Определено, что не хватает памяти в обе стороны, но возникла ошибка при копировании данных из старого сегмента памяти в новый. При этой ошибке новая память помечается свободной автоматически, изменения не принимаются в силу. Компиляция программы с технологией SDL.
+/// -9	memcpy: Определено, что не хватает памяти в обе стороны, но возникла ошибка при копировании данных из старого сегмента памяти в новый. При этой ошибке новая память помечается свободной автоматически, изменения не принимаются в силу.
+/// </return>
+int ExpandableLenta_MemoryAdjustment(struct Lenta * input);
+#else
+/* Проверка линии в машине Тьюринга, хватает ли места. Если не хватает, то идёт выделение новой памяти для ленты в машине Тьюринга. При неудаче возвращается отрицательный код ошибки. */
+int ExpandableLenta_MemoryAdjustment(struct Lenta * input);
+#endif
+int ExpandableLenta_MemoryAdjustment(struct Lenta * input)
+{
+	int flag = 0;
+	unsigned char * buffer;
+	if (input->position + 2 >= input->top)
+	{
+		flag += 1;
+	}
+	flag <<= 1;
+	if (input->position - 2 <= input->elements)
+	{
+		flag += 1;
+	}
+	switch (flag)
+	{
+		case 0: /* Везде всё хватает */
+		{
+			return 0;
+		}
+		case 1: /* Снизу не хватает */
+		{
+			buffer = (unsigned char*)malloc(sizeof(unsigned char)*(input->top - input->elements + 1 + 2));
+			if (buffer == NULL)
+			{
+				return -2;
+			}
+#if SDL == 1
+			if (memcpy_s(buffer + 2, input->top - input->elements, input->elements, input->top - input->elements) != 0)
+			{
+				free(buffer);
+				return -4;
+			}
+#else
+			if (memcpy(buffer + 2, input->elements, input->top - input->elements) == NULL)
+			{
+				free(buffer);
+				return -5;
+			}
+#endif
+			free(input->elements);													/* Помечаем, что старый информация из старого массива данных не нужна	*/
+			input->position = input->position - input->elements + buffer + 2;		/* Устанавливаем новую позицию											*/
+			input->top = buffer + (input->top - input->elements) + 2;				/* Устанавливаем указатель на последний доступный элемент				*/
+			input->elements = buffer;												/* Указываем, где наш новый участок памяти								*/
+																					/* Очищаем новые секторы:												*/
+			ExpandableLenta_GetText(*input, 'L');
+			buffer[0] = 0;
+			buffer[1] = 0;
+			input->top[0] = 0;
+			ExpandableLenta_GetText(*input, 'L');
+			return 1;
+		}
+		case 2: /* Сверху не хватает */
+		{
+			buffer = (unsigned char*)malloc(sizeof(unsigned char)*(input->top - input->elements + 1 + 2));
+			if (buffer == NULL)
+			{
+				return -2;
+			}
+#if SDL == 1
+			if (memcpy_s(buffer, input->top - input->elements + 2, input->elements, input->top - input->elements) != 0)
+			{
+				free(buffer);
+				return -6;
+			}
+#else
+			if (memcpy(buffer, input->elements, input->top - input->elements) == NULL)
+			{
+				free(buffer);
+				return -7;
+			}
+#endif
+			free(input->elements);													/* Помечаем, что старый информация из старого массива данных не нужна	*/
+			input->position = input->position - input->elements + buffer;			/* Устанавливаем новую позицию											*/
+			input->top = buffer + (input->top - input->elements) + 2;				/* Устанавливаем указатель на последний доступный элемент				*/
+			input->elements = buffer;												/* Указываем, где наш новый участок памяти								*/
+																					/* Очищаем новые секторы:												*/
+
+			input->top[0] = 0;
+			input->top[-1] = 0;
+			input->top[-2] = 0;
+			return 2;
+		}
+		case 3: /* Не хватает с двух сторон */
+		{
+			buffer = (unsigned char*)malloc(sizeof(unsigned char)*(input->top - input->elements + 1 + 4));
+			if (buffer == NULL)
+			{
+				return -3;
+			}
+#if SDL == 1
+			if (memcpy_s(buffer + 2, input->top - input->elements + 2, input->elements, input->top - input->elements) != 0)
+			{
+				free(buffer);
+				return -8;
+			}
+#else
+			if (memcpy(buffer + 2, input->elements, input->top - input->elements) == NULL)
+			{
+				free(buffer);
+				return -9;
+			}
+#endif
+			free(input->elements);													/* Помечаем, что старый информация из старого массива данных не нужна	*/
+			input->position = input->position - input->elements + buffer + 2;		/* Устанавливаем новую позицию											*/
+			input->top = buffer + (input->top - input->elements) + 4;				/* Устанавливаем указатель на последний доступный элемент				*/
+			input->elements = buffer;												/* Указываем, где наш новый участок памяти								*/
+																					/* Очищаем новые секторы:												*/
+			buffer[0] = 0;
+			buffer[1] = 0;
+			input->top[0] = 0;
+			input->top[-1] = 0;
+			input->top[-2] = 0;
+			return 3;
+		}
+		default: /* взлом системы? */
+			return -1;
+	}
+}
+
+#endif /* _MUWA_EXPANDABLE_LENTA */
